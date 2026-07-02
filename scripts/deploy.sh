d65982bc8340c3796c8230c9940643d003d5a6ff
taiatiniyara <taiatiniyara@gmail.com>
2026-07-02 19:38:26 +1200

    Deploy: source nvm for non-interactive SSH sessions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 if [ ! -d /var/www/vanuarai/.git ]; then
   rm -rf /var/www/vanuarai
   git clone https://darkhorsefiji:__GIT_TOKEN__@github.com/darkhorsefiji/vanuarai.git /var/www/vanuarai
@@ -7,6 +9,7 @@ fi
 cd /var/www/vanuarai
 git remote set-url origin https://darkhorsefiji:__GIT_TOKEN__@github.com/darkhorsefiji/vanuarai.git
 git pull origin main
+export PATH="$NVM_DIR/versions/node/$(node -v)/bin:$PATH"
 npm ci
 npm ci --prefix web
 npm run build
